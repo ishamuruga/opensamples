6d80b4d750b11bf43bcb0237eec85570607c1b53
SivaKumar <siva@example.com>
2023-02-02 20:43:45 +0530

    iv

--- flutter/flutter_myapp/lib/Login.dart
+++ flutter/flutter_myapp/lib/Login.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'package:flutter/material.dart';
+import 'HomePage.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -13,7 +12,7 @@ class LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.yellow,
+        backgroundColor: Colors.white,
         appBar: AppBar(
           title: Text("eMarket Place"),
         ),
@@ -24,11 +23,59 @@ class LoginState extends State<Login> {
               padding: EdgeInsets.only(top: 60.0),
               child: Center(
                   child: Container(
-                width: 200,
-                height: 150,
-                child: Image.asset('./asset/logo.png'),
+                width: 75,
+                height: 75,
+                child: Image.asset('assets/image/logo.png'),
               )),
-            )
+            ),
+            Padding(
+              padding:
+                  EdgeInsets.only(left: 15.0, right: 15.0, top: 15, bottom: 0),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                    hintText: 'Enter valid email id as abc@gmail.com'),
+              ),
+            ),
+            Padding(
+              padding:
+                  EdgeInsets.only(left: 15.0, right: 15.0, top: 15, bottom: 0),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                    hintText: 'Enter secure password'),
+              ),
+            ),
+            TextButton(
+              onPressed: () => {print('pressed')},
+              child: Text(
+                "Forgot Password",
+                style: TextStyle(color: Colors.blue, fontSize: 15),
+              ),
+            ),
+            Container(
+              height: 50,
+              width: 250,
+              decoration: BoxDecoration(
+                  color: Colors.blue, borderRadius: BorderRadius.circular(20)),
+              child: ElevatedButton(
+                onPressed: () => {
+                  Navigator.push(
+                      context, MaterialPageRoute(builder: (_) => HomePage()))
+                },
+                child: Text(
+                  "Login",
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 130,
+            ),
+            Text("New User? Create Account")
           ],
         )));
   }
